0e958f8e8d6ea964abb6721443f0c6cde5e72077
Thanh Truong <hopchuathurac@gmail.com>
2020-07-17 11:26:18 +0200

    backup MEXIMA site

---
+++ setenv.cmd
@@ -0,0 +1,12 @@
+set JDK=C:\ProgramFiles\Java\jdk1.6.0_22
+set AMOS_HOME=E:\AmosNT\
+
+REM system environments
+set PATH=C:\Windows\system32;C:\Windows;C:\ProgramFiles\Microsoft Visual Studio\Common\MSDev98\Bin;C:\ProgramFiles\Microsoft Visual Studio\VC98\Bin;D:\Softwares\gp426win32\gnuplot\bin;C:\Program Files (x86)\CVSNT\
+
+REM Path to system ones
+set PATH=%AMOS_HOME%bin;%AMOS_HOME%jarlib;%PYTHON_HOME%;%JDK%\bin;%PATH%
+set CLASSPATH=%AMOS_HOME%jarlib\mysql-connector-java-5.1.6-bin.jar;%AMOS_HOME%bin\javaamos.jar;C:\ProgramFiles\utils\mssqljdbc\mssqlserver.jar;C:\ProgramFiles\utils\mssqljdbc\msbase.jar;C:\ProgramFiles\utils\mssqljdbc\msutil.jar;C:\ProgramFiles\Java\jre6\lib\ext;.;kd.jar
+
+
+
